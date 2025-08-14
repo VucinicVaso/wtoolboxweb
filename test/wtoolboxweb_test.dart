c547501212f62ec2992d5ced8ae9517f0697c30f
vaso.vucinic <vasoct@gmail.com>
2025-08-14 12:15:36 +0200

    updated wtoolbox_test

--- test/wtoolboxweb_test.dart
+++ test/wtoolboxweb_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'component/empty/wtw_ui_empty_impl_test.dart' as empty_impl_component_test;
-import 'component/space/wtw_ui_space_basic_test.dart' as space_basic_component_test;
-import 'component/divider/wtw_ui_divider_horizontal_test.dart' as divider_horizontal_component_test;
-import 'component/button/wtw_ui_button_text_test.dart' as button_ui_button_test_test;
-import 'component/button/wtw_ui_button_underline_text_test.dart' as button_ui_button_underline_test_test;
+import 'component/empty/wtw_ui_empty_impl_test.dart' as component_empty_impl_test;
+import 'component/space/wtw_ui_space_basic_test.dart' as component_space_basic_test;
+import 'component/divider/wtw_ui_divider_horizontal_test.dart' as component_divider_horizontal_test;
+import 'component/button/wtw_ui_button_text_test.dart' as component_button_ui_button_text_test;
+import 'component/button/wtw_ui_button_underline_text_test.dart' as component_button_ui_button_underline_text_test;
 
 void main() {
   
@@ -12,10 +12,10 @@ void main() {
   //   () {}
   // );
 
-  empty_impl_component_test.main();
-  space_basic_component_test.main();
-  divider_horizontal_component_test.main();
-  button_ui_button_test_test.main();
-  button_ui_button_underline_test_test.main();
+  component_empty_impl_test.main();
+  component_space_basic_test.main();
+  component_divider_horizontal_test.main();
+  component_button_ui_button_text_test.main();
+  component_button_ui_button_underline_text_test.main();
 
 }
